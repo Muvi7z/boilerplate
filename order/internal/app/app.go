65f155fda84df16a1e30449dd075413d2641f0b4
Ochir.zha <ochir-zhamsuev@yandex.ru>
2025-12-24 18:07:12 +0800

    добавил контейнер 2

--- order/internal/app/app.go
+++ order/internal/app/app.go
@@ -2,13 +2,19 @@ package app
 
 import (
 	"context"
+	"github.com/Muvi7z/boilerplate/order/internal/client/grpc"
+	inventory "github.com/Muvi7z/boilerplate/order/internal/client/grpc/inventory/v1"
+	v1 "github.com/Muvi7z/boilerplate/order/internal/client/grpc/payment/v1"
 	"github.com/Muvi7z/boilerplate/order/internal/config"
+	inventory_v1 "github.com/Muvi7z/boilerplate/shared/pkg/proto/inventory/v1"
 	payment_v1 "github.com/Muvi7z/boilerplate/shared/pkg/proto/payment/v1"
 	grpc2 "google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 type App struct {
+	paymentService   grpc.PaymentClient
+	inventoryService grpc.InventoryClient
 }
 
 func New(ctx context.Context) (*App, error) {
@@ -35,7 +41,7 @@ func (a *App) initDeps(ctx context.Context) error {
 	return nil
 }
 
-func (a *App) InitGRPCClient(ctx context.Context) error {
+func (a *App) InitPaymentGRPCClient(ctx context.Context) error {
 	connPayment, err := grpc2.NewClient(
 		config.AppConfig().PaymentGRPC.Address(),
 		grpc2.WithTransportCredentials(insecure.NewCredentials()),
@@ -44,6 +50,24 @@ func (a *App) InitGRPCClient(ctx context.Context) error {
 		return err
 	}
 	paymentClient := payment_v1.NewPaymentClient(connPayment)
+
+	a.paymentService = v1.New(paymentClient)
+
+	return nil
+}
+
+func (a *App) InitInventoryGRPCClient(ctx context.Context) error {
+	connInventory, err := grpc2.NewClient(
+		config.AppConfig().InventoryGRPC.Address(),
+		grpc2.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return err
+	}
+
+	inventoryClient := inventory_v1.NewInventoryServiceClient(connInventory)
+
+	a.inventoryService = inventory.New(inventoryClient)
 
 	return nil
 }
